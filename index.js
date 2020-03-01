43fdc792b43d3fa4f305de85d582f49fa3e7130a
Carlos Eduardo <ceo.paludetto@gmail.com>
2020-03-01 19:30:19 -0300

    chore: adding eslint-disable comment

--- index.js
+++ index.js
@@ -1,93 +1,85 @@
-const path = require('path')
-const { readFile, writeFile } = require('fs')
-const { camelize } = require('humps')
-const { getOptions } = require('loader-utils')
+const path = require("path");
+const { readFile, writeFile } = require("fs");
+const { camelize } = require("humps");
+const { getOptions } = require("loader-utils");
 
-function getConfig (loader) {
-  const config = getOptions(loader) || {}
+const template = require("./lib/template");
+
+function getConfig(loader) {
+  const config = getOptions(loader) || {};
 
   return {
-    suffix: () => 'Styles',
+    suffix: () => "Styles",
     ...config
-  }
+  };
 }
 
-function capitalizeFirstLetter (s) {
-  return s.charAt(0).toUpperCase() + s.slice(1)
+function capitalizeFirstLetter(s) {
+  return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
-function stripExtension (s) {
-  return s.split('.')[0]
+function stripExtension(s) {
+  return s.split(".")[0];
 }
 
-function validateName (s) {
+function validateName(s) {
   if (/^[a-zA-Z]/.test(s) === false) {
-    return `I${s}`
+    return `I${s}`;
   }
 
-  return s
+  return s;
 }
 
-module.exports = function loader (webpackSource) {
-  this.cacheable()
+module.exports = function loader(webpackSource) {
+  this.cacheable();
 
-  const callback = this.async()
+  const callback = this.async();
 
   const end = err => {
     if (err) {
-      callback(err)
+      callback(err);
     } else {
-      callback(null, webpackSource)
+      callback(null, webpackSource);
     }
-  }
+  };
 
-  const match = webpackSource.match(/exports\.locals.*=([^]*?);/)
+  const match = webpackSource.match(/exports\.locals.*=([^]*?);/);
 
   if (match) {
-    const config = getConfig(this)
+    const config = getConfig(this);
 
-    const locals = []
+    const locals = [];
 
-    match[1].split('\n').map(s => {
-      const keyMatch = s.match(/"(.*?)": /)
+    match[1].split("\n").map(s => {
+      const keyMatch = s.match(/"(.*?)": /);
 
       if (keyMatch) {
-        locals.push(keyMatch[1])
+        locals.push(keyMatch[1]);
       }
-    })
+    });
 
     if (locals.length) {
-      const suffix = config.suffix(this.resource)
+      const suffix = config.suffix(this.resource);
       const name = `${validateName(
         capitalizeFirstLetter(
           camelize(stripExtension(path.basename(this.resource)))
         )
-      )}${suffix}`
+      )}${suffix}`;
 
-      const output = `/* tslint:disable */
-// This file was automatically generated and should not be edited.
-export interface ${name} {
-  [index: string]: string;
-${locals.map(local => `\t'${local}': string;`).join('\n')}
-}
-
-export type I${name} = ${name};
-export const locals: ${name};
-export default locals;
-`
-      const filename = `${this.resource}.d.ts`
+      const output = template(name, locals);
+      const filename = `${this.resource}.d.ts`;
 
       readFile(filename, (err, data) => {
         if (err || data !== output) {
-          writeFile(filename, output, end)
+          writeFile(filename, output, end);
         } else {
-          end()
+          end();
         }
-      })
+      });
     } else {
-      end()
+      end();
     }
   } else {
-    end()
+    end();
   }
-}
+};
